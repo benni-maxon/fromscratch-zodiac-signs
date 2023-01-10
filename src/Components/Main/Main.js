52558d1db99f9514b813db75891cd9d9c6e09eec
benni maxon <benni.maxon@gmail.com>
2023-01-10 15:50:41 -0800

    created component js files

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import background from '../../astro-van.png';
+
+export default function Main() {
+  return <main style={{ backgroundImage: `url(${background})` }}>Main</main>;
+}
