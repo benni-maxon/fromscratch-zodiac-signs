d3a1efb477611718d34d97dc8dd8bbf32c13be1b
benni maxon <benni.maxon@gmail.com>
2023-01-10 19:31:58 -0800

    styling and linking style sheets

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import './Header.css';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <span>Chevrolet </span>Astro <span>Van</span> Signs
+    </div>
+  );
 }
