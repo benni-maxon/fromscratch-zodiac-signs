d486859d360312b3f3b1d09028ad1efa458001f8
benni maxon <benni.maxon@gmail.com>
2023-01-10 19:31:35 -0800

    added .map loop for Zodiac component

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import { zodiac } from '../../data.js';
 import background from '../../astro-van.png';
+import Zodiac from '../Zodiac/Zodiac.js';
+import './Main.css';
 
 export default function Main() {
-  return <main style={{ backgroundImage: `url(${background})` }}>Main</main>;
+  return (
+    <main className="main-area" style={{ backgroundImage: `url(${background})` }}>
+      {zodiac.map((item) => (
+        <Zodiac key={item.id} {...item} />
+      ))}
+    </main>
+  );
 }
